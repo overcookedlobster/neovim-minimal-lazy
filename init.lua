d80c5ca3530524103547b4300f89bb3073dea4e3
Yahwista Salomo <yahwistasalomo@gmail.com>
2025-08-28 13:54:47 +0800

    change requires

--- init.lua
+++ init.lua
@@ -9,8 +9,31 @@ vim.g.maplocalleader = "\\"
 local config_dir = vim.fn.stdpath("config")
 package.path = config_dir .. "/?.lua;" .. config_dir .. "/?/init.lua;" .. package.path
 
--- Bootstrap lazy.nvim plugin manager
-require("config.lazy")
+-- Skip lazy.nvim bootstrap since plugins are already installed
+-- and the auto-installation is causing the "Too many rounds" error
+local lazypath = "/nfs/iil/proj/mpg/sa_09/chip_design/projects/usb2/sonora3/ysalomox/nvim-plugins/lazy/lazy.nvim"
+
+-- Only try to load lazy.nvim if it exists, don't auto-install
+if vim.loop.fs_stat(lazypath) then
+  vim.opt.rtp:prepend(lazypath)
+
+  -- Try to load lazy.nvim safely
+  local ok, lazy = pcall(require, "lazy")
+  if ok then
+    print("Loading existing plugins...")
+    -- Just load existing plugins, don't try to install new ones
+    lazy.setup({}, {
+      root = "/nfs/iil/proj/mpg/sa_09/chip_design/projects/usb2/sonora3/ysalomox/nvim-plugins",
+      install = { missing = false }, -- Don't auto-install missing plugins
+      checker = { enabled = false }, -- Disable update checker
+      change_detection = { enabled = false }, -- Disable file watching
+    })
+  else
+    print("Lazy.nvim found but failed to load, skipping plugin manager...")
+  end
+else
+  print("Lazy.nvim not found, using built-in features only...")
+end
 
 -- Load core configuration
 require("config.options")
